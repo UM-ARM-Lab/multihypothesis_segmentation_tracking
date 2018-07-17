d484cd932a87a4724ad1cc2a5c2b90b719a493bb
Andrew Price <arprice@gatech.edu>
2018-07-17 14:30:05 -0400

    Filtering out table frame to cluster objects.

--- src/shape_completion_manager.cpp
+++ src/shape_completion_manager.cpp
@@ -1,6 +1,7 @@
 #include <octomap/octomap.h>
 #include <octomap_msgs/conversions.h>
 #include <octomap_msgs/GetOctomap.h>
+#include <Eigen/Geometry>
 
 #include <std_msgs/ByteMultiArray.h>
 
@@ -13,20 +14,15 @@ namespace om = octomap;
 ros::ServiceClient mapClient;
 ros::Publisher localMapPub;
 
-void getOccupancy(const om::point3d& min, const om::point3d& max, const int resolution = 64)
+std::pair<std::shared_ptr<om::OcTree>, std::string> getOctree()
 {
-	assert(resolution > 0);
-	assert(max.x() > min.x());
-	assert(max.y() > min.y());
-	assert(max.z() > min.z());
-
 	octomap_msgs::GetOctomapRequest req;
 	octomap_msgs::GetOctomapResponse resp;
 	bool callSucceeded = mapClient.call(req, resp);
 	if (!callSucceeded)
 	{
 		ROS_ERROR("Unable to call Octomap service.");
-		return;
+		return {std::shared_ptr<om::OcTree>(), resp.map.header.frame_id};
 	}
 
 	std::shared_ptr<octomap::AbstractOcTree> abstractTree(octomap_msgs::msgToMap(resp.map));
@@ -35,8 +31,29 @@ void getOccupancy(const om::point3d& min, const om::point3d& max, const int reso
 	if (!octree)
 	{
 		ROS_ERROR("Unable to downcast abstract octree to concrete tree.");
-		return;
+		return {std::shared_ptr<om::OcTree>(), resp.map.header.frame_id};
 	}
+
+	return {octree, resp.map.header.frame_id};
+}
+
+// min and max now specified in camera frame
+void getOccupancy(const Eigen::Vector3f& min, const Eigen::Vector3f& max, const Eigen::Affine3f& worldTcamera, std::shared_ptr<om::OcTree>& octree, const int resolution = 64)
+{
+	assert(resolution > 0);
+	assert(max.x() > min.x());
+	assert(max.y() > min.y());
+	assert(max.z() > min.z());
+
+//	// Get Octree
+//	auto octreeInfo = getOctree();
+//	std::shared_ptr<om::OcTree> octree = octreeInfo.first;
+//	std::string globalFrame = octreeInfo.second;
+//	if (!octree)
+//	{
+//		ROS_ERROR("Octree lookup failed.");
+//		return;
+//	}
 
 	std::cerr << octree->getNumLeafNodes() << ", " << octree->getResolution() << std::endl;
 
@@ -55,15 +72,17 @@ void getOccupancy(const om::point3d& min, const om::point3d& max, const int reso
 	int unknownCount = 0;
 	for (int i = 0; i < resolution; ++i)
 	{
-		double x = min.x() + (max.x() - min.x())*(i/static_cast<double>(resolution-1));
+		float x = min.x() + (max.x() - min.x())*(i/static_cast<float>(resolution-1));
 		for (int j = 0; j < resolution; ++j)
 		{
-			double y = min.y() + (max.y() - min.y())*(j/static_cast<double>(resolution-1));
+			float y = min.y() + (max.y() - min.y())*(j/static_cast<float>(resolution-1));
 			for (int k = 0; k < resolution; ++k)
 			{
-				double z = min.z() + (max.z() - min.z())*(k/static_cast<double>(resolution-1));
+				float z = min.z() + (max.z() - min.z())*(k/static_cast<float>(resolution-1));
 
-				om::OcTreeNode* node = octree->search(x, y, z);
+				Eigen::Vector3f queryPoint = worldTcamera * Eigen::Vector3f(x, y, z);
+
+				om::OcTreeNode* node = octree->search(queryPoint.x(), queryPoint.y(), queryPoint.z());
 				if (!node)
 				{
 					//This cell is unknown
@@ -74,16 +93,16 @@ void getOccupancy(const om::point3d& min, const om::point3d& max, const int reso
 				{
 					double cellValue = node->getOccupancy() - 0.5;
 
-					// Temp code for testing features
-					for (unsigned int d = 0; d < octree->getTreeDepth(); ++d)
-					{
-						om::OcTreeKey key = octree->coordToKey(x, y, z, d);
-						octree->search(key, d);
-						octree->search(x, y, z, d);
-						octree->getTreeType();
-						octree->getNodeSize(d);
-						om::point3d p = octree->keyToCoord(key, d);
-					}
+//					// Temp code for testing features
+//					for (unsigned int d = 0; d < octree->getTreeDepth(); ++d)
+//					{
+//						om::OcTreeKey key = octree->coordToKey(x, y, z, d);
+//						octree->search(key, d);
+//						octree->search(x, y, z, d);
+//						octree->getTreeType();
+//						octree->getNodeSize(d);
+//						om::point3d p = octree->keyToCoord(key, d);
+//					}
 
 					if (cellValue > 0)
 					{
@@ -108,10 +127,178 @@ void getOccupancy(const om::point3d& min, const om::point3d& max, const int reso
 	localMapPub.publish(arrayMsg);
 }
 
+// Point cloud utilities
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl_ros/transforms.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/crop_box.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/features/moment_of_inertia_estimation.h>
+#include <tf_conversions/tf_eigen.h>
+#include <boost/core/null_deleter.hpp>
+//#include <abstract.h>
+
+using PointT = pcl::PointXYZRGB;
+
+std::shared_ptr<tf::TransformListener> listener;
+
+template <typename PointT>
+void getAABB(const pcl::PointCloud<PointT>& members, Eigen::Vector3f& min, Eigen::Vector3f& max)
+{
+	const int DIM = 3;
+	for (int d=0; d < DIM; ++d)
+	{
+		min[d] = std::numeric_limits<float>::infinity();
+		max[d] = -std::numeric_limits<float>::infinity();
+	}
+
+	for (const PointT& p : members)
+	{
+		for (int d=0; d < DIM; ++d)
+		{
+			min[d] = std::min(min[d], p.getVector3fMap()[d]);
+			max[d] = std::max(max[d], p.getVector3fMap()[d]);
+		}
+	}
+}
+
+void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloudMsg)
+{
+	// Get Octree
+	auto octreeInfo = getOctree();
+	std::shared_ptr<om::OcTree> octree = octreeInfo.first;
+	std::string globalFrame = octreeInfo.second;
+	if (!octree)
+	{
+		ROS_ERROR("Octree lookup failed.");
+		return;
+	}
+
+	// Transform point cloud to local frame
+	if (!listener->waitForTransform(globalFrame, cloudMsg->header.frame_id, ros::Time(0), ros::Duration(5.0)))
+	{
+		ROS_WARN_STREAM("Failed to look up transform between '" << globalFrame << "' and '" << cloudMsg->header.frame_id << "'.");
+		return;
+	}
+//	sensor_msgs::PointCloud2 transformedCloud;
+//	pcl_ros::transformPointCloud(globalFrame, *cloudMsg, transformedCloud, *listener);
+
+	pcl::PointCloud<PointT>::Ptr cloud(new pcl::PointCloud<PointT>());
+//	pcl::fromROSMsg(transformedCloud, *cloud);
+	pcl::fromROSMsg(*cloudMsg, *cloud);
+
+	// Filter box
+	pcl::PointCloud<PointT>::Ptr cropped_cloud(new pcl::PointCloud<PointT>());
+	// Coordinates in table ("global") frame
+	Eigen::Vector4f maxExtent(0.5, 1, 1, 1);
+	Eigen::Vector4f minExtent(-0.5, -1, -0.25, 1);
+
+	tf::StampedTransform tableFrameInCameraCoordinates;
+	listener->lookupTransform(globalFrame, cloudMsg->header.frame_id, ros::Time(0), tableFrameInCameraCoordinates);
+	Eigen::Affine3d tablePose;
+	tf::transformTFToEigen(tableFrameInCameraCoordinates, tablePose);
+
+	pcl::CropBox<PointT> boxFilter;
+	boxFilter.setMin(minExtent);
+	boxFilter.setMax(maxExtent);
+	boxFilter.setTransform(tablePose.cast<float>());
+	boxFilter.setInputCloud(cloud);
+	boxFilter.filter(*cropped_cloud);
+
+	std::cerr << "Filtered points: " << cropped_cloud->size() << std::endl;
+
+	if (cropped_cloud->empty())
+	{
+		ROS_WARN("Filtered cloud contains no points.");
+		return;
+	}
+
+	pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+	pcl::PointIndices::Ptr table_inliers (new pcl::PointIndices);
+	// Create the segmentation object
+	pcl::SACSegmentation<PointT> seg;//pcl::SACSegmentation<pcl::PointXYZ> seg;
+	// Optional
+	seg.setOptimizeCoefficients (true);
+	// Mandatory
+	seg.setModelType (pcl::SACMODEL_PLANE);
+	seg.setMethodType (pcl::SAC_RANSAC);
+	seg.setDistanceThreshold (0.01); //octree->getResolution();
+
+	seg.setInputCloud (cropped_cloud);
+	seg.segment (*table_inliers, *coefficients);
+
+	if (table_inliers->indices.size () == 0)
+	{
+		ROS_WARN("Could not estimate a planar model for the given dataset.");
+		return;
+	}
+
+	pcl::ExtractIndices<PointT> extractor;
+	extractor.setInputCloud(cropped_cloud);
+	extractor.setIndices(table_inliers);
+	extractor.setNegative(true);
+
+	pcl::PointCloud<PointT>::Ptr non_table_cloud(new pcl::PointCloud<PointT>());
+	extractor.filter(*non_table_cloud);
+
+	pcl::search::KdTree<PointT>::Ptr tree (new pcl::search::KdTree<PointT>);
+	tree->setInputCloud(non_table_cloud);
+
+	std::vector<pcl::PointIndices> clusters;
+	pcl::EuclideanClusterExtraction<PointT> ec;
+	ec.setClusterTolerance (0.02); // 2cm
+	ec.setMinClusterSize (100);
+	ec.setMaxClusterSize (25000);
+	ec.setSearchMethod (tree);
+	ec.setInputCloud (non_table_cloud);
+	ec.extract (clusters);
+
+	for (const pcl::PointIndices& cluster : clusters)
+	{
+		pcl::ExtractIndices<PointT> cluster_extractor;
+		cluster_extractor.setInputCloud(non_table_cloud);
+		cluster_extractor.setIndices(pcl::PointIndices::ConstPtr(&cluster, boost::null_deleter()));
+		cluster_extractor.setNegative(false);
+
+		pcl::PointCloud<PointT>::Ptr cluster_cloud(new pcl::PointCloud<PointT>());
+		cluster_extractor.filter(*cluster_cloud);
+
+		// Compute bounding box
+//		PointT p; p.getVector3fMap()
+		Eigen::Vector3f min, max;
+		getAABB(*cluster_cloud, min, max);
+		Eigen::Vector3f sides = max-min;
+		float max_len = sides.array().maxCoeff();
+
+		for (int d=0; d < 3; ++d)
+		{
+			float delta = max_len - sides[d];
+			assert(delta > -1e-9);
+			if (delta > 0)
+			{
+				max[d] += delta/2.0;
+				min[d] -= delta/2.0;
+			}
+		}
+
+		getOccupancy(min, max, tablePose.cast<float>(), octree);
+	}
+}
+
 int main(int argc, char* argv[])
 {
 	ros::init(argc, argv, "shape_completion_manager");
 	ros::NodeHandle nh;
+
+	listener = std::make_shared<tf::TransformListener>();
 
 	localMapPub = nh.advertise<std_msgs::ByteMultiArray>("local_occupancy", 1, true);
 	mapClient = nh.serviceClient<octomap_msgs::GetOctomap>("/octomap_binary");
@@ -120,9 +307,12 @@ int main(int argc, char* argv[])
 		ROS_WARN("Map server not connected.");
 	}
 
-	getOccupancy(om::point3d(-0.1f, -0.1f, -0.1f), om::point3d(0.1f, 0.1f, 0.1f));
+	ros::Subscriber sub = nh.subscribe ("kinect2_victor_head/qhd/points", 1, cloud_cb);
 
-	ros::spinOnce();
+//	getOccupancy(om::point3d(-0.1f, -0.1f, -0.1f), om::point3d(0.1f, 0.1f, 0.1f));
+
+//	ros::spinOnce();
+	ros::spin();
 
 	return 0;
 }
