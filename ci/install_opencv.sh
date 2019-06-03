d6745730dfdad803ceb42b42200265ae3ae9a04d
Kun Huang <huangkun@umich.edu>
2019-06-03 16:04:43 -0400

    install opencv modified

--- ci/install_opencv.sh
+++ ci/install_opencv.sh
@@ -8,6 +8,6 @@ git clone --branch 3.4.5 --depth 1 https://github.com/opencv/opencv_contrib.git
 cd opencv
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=~/local -DCMAKE_BUILD_TYPE=Release -DOPENCV_EXTRA_MODULES_PATH=~/local/src_repos/opencv_contrib/modules -DOPENCV_ENABLE_NONFREE=ON -DWITH_CUDA=ON -DBUILD_EXAMPLES=OFF -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_JAVA=OFF ..
+cmake -DCMAKE_INSTALL_PREFIX=~/local -DCMAKE_BUILD_TYPE=Release -DOPENCV_EXTRA_MODULES_PATH=~/local/src_repos/opencv_contrib/modules -DOPENCV_ENABLE_NONFREE=ON -DWITH_CUDA=ON -DBUILD_EXAMPLES=OFF -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_JAVA=OFF -DBUILD_opencv_cudacodec=OFF ..
 make -j$(nproc)
 make install
