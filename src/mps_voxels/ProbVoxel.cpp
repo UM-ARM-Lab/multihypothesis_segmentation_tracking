7ade02c509a9b8b9edbcac89bcd032a7673f17e6
Kun Huang <huangkun@umich.edu>
2020-01-03 23:42:14 -0500

    prepare for prob scene understanding

---
+++ src/mps_voxels/ProbVoxel.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kunhuang on 10/23/19.
+//
+
+#include "ProbVoxel.h"
---
+++ src/mps_voxels/ProbVoxel.h
@@ -0,0 +1,8 @@
+//
+// Created by kunhuang on 10/23/19.
+//
+
+#ifndef ARMLAB_WS_PROBVOXEL_H
+#define ARMLAB_WS_PROBVOXEL_H
+
+#endif //ARMLAB_WS_PROBVOXEL_H
