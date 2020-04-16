f250b71993607815f02a355bf38ddda3380e3f3d
Andrew Price <arprice@gatech.edu>
2020-04-15 20:43:17 -0400

    Docker/CI updates.

--- ci/install_opencv.sh
+++ ci/install_opencv.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env bash
 
 # Install OpenCV from source
-mkdir -p ~/local/src_repos
-cd ~/local/src_repos
-git clone --branch 3.4.5 --depth 1 https://github.com/opencv/opencv.git
-git clone --branch 3.4.5 --depth 1 https://github.com/opencv/opencv_contrib.git
+SRC_DIR=~/local/src_repos
+mkdir -p ${SRC_DIR}
+cd ${SRC_DIR}
+git clone --branch 3.4.9 --depth 1 https://github.com/opencv/opencv.git
+git clone --branch 3.4.9 --depth 1 https://github.com/opencv/opencv_contrib.git
 cd opencv
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=~/local -DCMAKE_BUILD_TYPE=Release -DOPENCV_EXTRA_MODULES_PATH=~/local/src_repos/opencv_contrib/modules -DOPENCV_ENABLE_NONFREE=ON -DWITH_CUDA=ON -DBUILD_EXAMPLES=OFF -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_JAVA=OFF -DBUILD_opencv_cudacodec=OFF ..
+cmake \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_PREFIX=~/local \
+	-DOPENCV_EXTRA_MODULES_PATH=${SRC_DIR}/opencv_contrib/modules \
+	-DOPENCV_ENABLE_NONFREE=ON \
+	-DWITH_CUDA=ON \
+	-DBUILD_opencv_cudacodec=OFF \
+	-DBUILD_EXAMPLES=OFF \
+	-DBUILD_TESTS=OFF \
+	-DBUILD_PERF_TESTS=OFF \
+	-DBUILD_JAVA=OFF \
+	-DCPACK_BINARY_DEB:BOOL=ON \
+	..
 make -j$(nproc)
 make install
