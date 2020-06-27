8cdd193f4316413eddccd9303889ea8c0c124834
Kun Huang <huangkun@umich.edu>
2020-06-27 13:51:16 -0400

    add ExperimentDir

---
+++ mps_voxels/include/mps_voxels/ExperimentDir.h
@@ -0,0 +1,18 @@
+//
+// Created by kunhuang on 6/27/20.
+//
+
+#ifndef SRC_EXPERIMENTDIR_H
+#define SRC_EXPERIMENTDIR_H
+
+#include <string>
+#include <vector>
+
+class ExperimentDir
+{
+public:
+	static const std::vector<std::string> checkpoints;
+	static const size_t bestGuessIndex;
+};
+
+#endif //SRC_EXPERIMENTDIR_H
---
+++ mps_voxels/src/mps_voxels/ExperimentDir.cpp
@@ -0,0 +1,7 @@
+//
+// Created by kunhuang on 6/27/20.
+//
+#include "mps_voxels/ExperimentDir.h"
+
+const std::vector<std::string> ExperimentDir::checkpoints = {"0_measurement", "1_mixture", "2_estimate", "3_motion", "4_freespace_refined"};
+const size_t ExperimentDir::bestGuessIndex = 2;
